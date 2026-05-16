634a93cc9aa4bed16b689bb2c9861aa3f2cfb19c
JDOT <JDON219206@gmail.com>
2026-05-16 15:50:48 +1000

    fix: fixed bugs with insert

--- HashTable.py
+++ HashTable.py
@@ -8,14 +8,12 @@ class HashTable:
     def calc_hash(self, key):
         total = sum(ord(char) for char in key)
         index = total % self.size 
-        if index < 0 or index > self.size:
-            return False
-        else:
-            return index
+        return index
         
     def rehash(self):
-        self.size = self.size * 2
         old_table = self.table
+        old_size = self.size
+        self.size = old_size * 2
         self.table = [None] * self.size
         self.count = 0
 
@@ -25,6 +23,27 @@ class HashTable:
                 value = item[1]
                 self.insert(key, value)
         return
+    
+    def direct_insert(self, key, value):
+        index = self.calc_hash(key)
+        probe = 0
+
+        while probe < self.size:
+            item = self.table[index]
+            if item is None:
+                self.table[index] = (key, value)
+                self.count += 1
+                return
+            elif item[0] == key:
+                self.table[index] = (key, value)
+                return
+            else:
+                index = (index + 1) % self.size
+                probe += 1
+
+    
+    def indexError(self):
+        print(f"index < 0 or index > {self.size}")
         
     def insert(self, key, value):
         load_factor = self.count / self.size
@@ -32,7 +51,7 @@ class HashTable:
             self.rehash()
         
         index = self.calc_hash(key)
-
+        
         while True:
             item = self.table[index]
             if item is None:
@@ -40,7 +59,7 @@ class HashTable:
                 self.count += 1
                 return
             elif item[0] == key:
-                item = (key, value)
+                self.table[index] = (key, value)
                 return
             else:
                 index = (index + 1) % self.size
@@ -51,9 +70,13 @@ class HashTable:
             return
         
         index = self.calc_hash(search_key)
-        probs = 0
+        if index < 0 or index > self.size:
+            self.indexError()
+            return
         
-        while True and probs < len(self.table):
+        probe = 0
+        
+        while True and probe < len(self.table):
             if self.table[index] is None:
                 print("Not found")
                 return
@@ -62,26 +85,34 @@ class HashTable:
                 return
             else:
                 index = (index + 1) % len(self.table) #probe
-                probs += 1
+                probe += 1
+        print("Key is not in the table")
 
-            
-        
-    
     def delete(self, delete_key):
         if self.table is None:
             print("Table is empty")
             return
         
         index = self.calc_hash(delete_key)
-        if index < 0 or index >= len(self.table):
-            print("Index out of bound")
+        if index < 0 or index > self.size:
+            self.indexError()
             return
-        
-        if self.table[index][0] == delete_key:
-            self.table[index] = None
-            self.count -= 1
-            print(f"{delete_key} successfully deleted")
-        return
+        probe = 0
+
+        while True and probe < len(self.table):
+            if self.table[index] is None:
+                print("Key is not in the table")
+                return
+            elif self.table[index][0] == delete_key:
+                self.table[index] = ("DELETED", "DELETED")
+                self.count -= 1
+                print(f"{delete_key} successfully deleted")
+                return
+            else:
+                index = (index + 1) % self.size
+                probe += 1
+        #if not found
+        print("Key is not in the table")
 
     def print_all(self):
         for item in self.table:
